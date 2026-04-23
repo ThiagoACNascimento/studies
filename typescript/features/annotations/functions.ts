b8bad39573f502c301ddd4787f6424189644ae0f
Thiago Costa <thiagoacnascimento@gmail.com>
2026-04-23 11:36:36 -0300

    destruction with annotations

--- typescript/features/annotations/functions.ts
+++ typescript/features/annotations/functions.ts
@@ -21,3 +21,21 @@ const logger = (message: string): void => {
 const throwError = (message: string): never => {
   throw new Error(message);
 };
+
+const todaysWeather = {
+  date: new Date(),
+  weather: 'sunny',
+};
+
+const logWather = ({
+  date,
+  weather,
+}: {
+  date: Date;
+  weather: string;
+}): void => {
+  console.log(date);
+  console.log(weather);
+};
+
+logWather(todaysWeather);
