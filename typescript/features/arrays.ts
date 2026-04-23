acabe943e94c2a0e785cd3d1119f7ad022120b4c
Thiago Costa <thiagoacnascimento@gmail.com>
2026-04-23 12:25:11 -0300

    multiple types

--- typescript/features/arrays.ts
+++ typescript/features/arrays.ts
@@ -8,9 +8,15 @@ const car = carMakers[0];
 const myCar = carMakers.pop();
 
 // Prevent incompatible values
-carMakers.push(100);
+carMakers.push(100); // ERROR
 
 // Help with 'map'
 carMakers.map((car: string): string => {
   return car;
 });
+
+// Flexible Types
+const importantDates: (Date | string)[] = [new Date(), '2026-04-23'];
+importantDates.push('2023-10-10');
+importantDates.push(new Date());
+importantDates.push(100); // ERROR
