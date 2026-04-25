4680e00dcb80a747e156ace9a62a4f16f468c622
Thiago Costa <thiagoacnascimento@gmail.com>
2026-04-25 18:57:41 -0300

    Classes

---
+++ typescript/features/classes.ts
@@ -0,0 +1,13 @@
+class Vihicle {
+  drive(): void {
+    console.log('vruuuuum');
+  }
+
+  honk(): void {
+    console.log('beee');
+  }
+}
+
+const vihicle = new Vihicle();
+vihicle.drive();
+vihicle.honk();
