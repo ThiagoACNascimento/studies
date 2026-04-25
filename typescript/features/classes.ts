520b34777c4dbd2dd4e6a9c9ede29f2273cd4957
Thiago Costa <thiagoacnascimento@gmail.com>
2026-04-25 20:15:41 -0300

    fields with inheritance

--- typescript/features/classes.ts
+++ typescript/features/classes.ts
@@ -12,6 +12,13 @@ class Vihicle {
 
 // `drive` can be only used inside the Vehicle Class
 class Car extends Vihicle {
+  constructor(
+    public wheels: number,
+    color?: string,
+  ) {
+    super(color);
+  }
+
   private drive(): void {
     console.log('vrum vrum....');
   }
@@ -29,5 +36,5 @@ console.log(vihicle.color);
 // cannot use `honk` outside the Vihicle Class
 // vihicle.honk();
 
-const car = new Car('black');
+const car = new Car(4, 'black');
 car.startDrive();
