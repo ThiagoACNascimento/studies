d65b2d68e9beb9b93d6231ff9f8dd74481362c9b
Thiago Costa <thiagoacnascimento@gmail.com>
2026-04-25 23:39:53 -0300

    add maps

--- typescript/maps/src/index.ts
+++ typescript/maps/src/index.ts
@@ -1,5 +1,16 @@
 import { User } from './User';
+import { Comapany } from './Company';
 
 const user = new User();
+const company = new Comapany();
 
 console.log(user);
+console.log(company);
+
+new google.maps.Map(document.getElementById('map') as HTMLElement, {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
